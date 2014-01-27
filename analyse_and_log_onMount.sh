ca46a900f1e978e25790089b0b620ae5eeb4532c
ramkam <contact@ramkam.com>
2014-01-27 12:53:39 +0100

    fixes + uuid AND protocol (usb, disk image, etc)

--- analyse_and_log_onMount.sh
+++ analyse_and_log_onMount.sh
@@ -11,10 +11,15 @@ mkdir -p "$diskdescdir";
 
 
 # Log disk usage & Update disk description
-df -h | grep "Volumes" | grep -v "MobileBackups" | awk '{print $2"|"$3"|"$4"|"$5"|"$6}' | while read l; do 
+df -h | grep "Volumes" | grep -v "MobileBackups" | while read ll; do 
+	b="$(echo "$ll" | awk '{print $2"|"$3"|"$4"|"$5}')"
+	a="$(echo "${ll:65}")"
+	l="$b|$a"
 	mountpoint="$(echo $l | awk -F"|" '{print $5}')";
+	echo "$mountpoint";
 	uuid="$(diskutil info "$mountpoint" | grep UUID | awk '{print $3}')"
-	diskutil info "$mountpoint" > "$diskdescdir/$uuid.txt"
-	echo "$ts|$uuid|$l" >> "$logusage"
+	protocol="$(diskutil info "$mountpoint" | grep -iF "Protocol:" | head -1 | awk '{print $2" "$3" "$4}')"
+	diskutil info "$mountpoint" > "$diskdescdir/Disk_${protocol}_${uuid}.txt"
+	echo "$ts|$protocol|$uuid|$l" >> "$logusage"
 done
 	
